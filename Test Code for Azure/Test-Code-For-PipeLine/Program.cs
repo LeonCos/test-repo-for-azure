1a4ab62a787a3f694f84c598f6108fe1e2f13ea2
LeonCos <50870555+LeonCos@users.noreply.github.com>
2019-05-22 10:14:17 +0200

    Added a folder with a test code in it
    Just added a Folder with a test Code in it.
    For testing the Connection between git and azure pipelines

---
+++ Test Code for Azure/Test-Code-For-PipeLine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Code_For_PipeLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Test Code for Azure");
+
+            int a = 4;
+            int b = 5;
+
+            Console.WriteLine("Simple Math" + a + b);
+        }
+    }
+}
